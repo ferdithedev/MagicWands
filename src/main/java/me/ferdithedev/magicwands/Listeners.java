0b1e6837ac8303c7dff0251c5c40afb1bb17f7dc
ferdithedev <linastomsuden@outlook.de>
2023-04-24 15:57:08 +0200

    Avifors effect now stackable and hat fixed

---
+++ src/main/java/me/ferdithedev/magicwands/Listeners.java
@@ -0,0 +1,14 @@
+package me.ferdithedev.magicwands;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+public class Listeners implements Listener {
+
+    @EventHandler
+    public void onInvClick(InventoryClickEvent e) {
+
+    }
+
+}
--- src/main/java/me/ferdithedev/magicwands/items/Avifors.java
+++ src/main/java/me/ferdithedev/magicwands/items/Avifors.java
@@ -4,6 +4,7 @@ import me.ferdithedev.overblock.games.ItemSpawner;
 import me.ferdithedev.overblock.obitems.OBItem;
 import me.ferdithedev.overblock.obitems.OBItemRarity;
 import me.ferdithedev.overblock.obitems.OBItemType;
+import me.ferdithedev.overblock.util.invs.InventoryItemCreator;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -14,7 +15,12 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Avifors extends OBItem {
+
+    private static final Map<Player, Integer> birdTime = new HashMap<>();
 
     public Avifors(JavaPlugin plugin) {
         super(plugin, Material.FEATHER, "Avifors", 0, OBItemType.TOOL, OBItemRarity.UNIQUE, "§7Transform yourself into a bird for 3 seconds");
@@ -22,23 +28,12 @@ public class Avifors extends OBItem {
 
     @Override
     public boolean function(Player player) {
-        player.setAllowFlight(true);
-        player.setFlying(true);
-        player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,60,5,false,false,false));
-        player.getInventory().setHelmet(ItemSpawner.getSkull("https://textures.minecraft.net/texture/ebee642cb4b4fb6671dec9ab9349f0c1ca83bc152738956c1efa2ff2c1030960"));
-
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                if(player.getGameMode() != GameMode.CREATIVE) {
-                    player.setAllowFlight(false);
-                    player.setFlying(false);
-                }
-
-                player.getInventory().setHelmet(new ItemStack(Material.AIR));
-            }
-        }.runTaskLaterAsynchronously(getPlugin(),60);
-
+        if(birdTime.containsKey(player)) {
+            birdTime.put(player,birdTime.get(player)+3);
+        } else {
+            birdTime.put(player,3);
+        }
+        if(!checkFly) checkFly();
         return true;
     }
 
@@ -47,5 +42,38 @@ public class Avifors extends OBItem {
         function(e.getPlayer());
         e.getPlayer().getInventory().removeItem(getItemStack());
         e.setCancelled(true);
+    }
+
+    boolean checkFly = false;
+    private void checkFly() {
+        checkFly = true;
+        for(Player p : birdTime.keySet()) {
+            if(birdTime.get(p) <= 0) {
+                birdTime.remove(p);
+                if(p.getGameMode() != GameMode.CREATIVE) {
+                    p.setAllowFlight(false);
+                    p.setFlying(false);
+                }
+
+                p.getInventory().setHelmet(new ItemStack(Material.AIR));
+            } else {
+                birdTime.put(p,birdTime.get(p)-1);
+
+                p.setAllowFlight(true);
+                p.setFlying(true);
+                p.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY,22,5,false,false,false));
+                p.getInventory().setHelmet(new InventoryItemCreator(ItemSpawner.getSkull("https://textures.minecraft.net/texture/ebee642cb4b4fb6671dec9ab9349f0c1ca83bc152738956c1efa2ff2c1030960")).setDisplayName("§dBirdie").get());
+            }
+        }
+        if(!birdTime.isEmpty()) {
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    checkFly();
+                }
+            }.runTaskLater(getPlugin(),20);
+        } else {
+            checkFly = false;
+        }
     }
 }
